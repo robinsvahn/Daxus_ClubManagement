734b72d61bcd2f83f6c9ed1c52d6521d2481fef7
Robin Svahn <robin.svahn@noser.com>
2017-10-31 16:08:02 +0100

    Added DAL project and Guest model class

---
+++ Daxus_ClubManagement_Core#2/Daxus_ClubManagement.DAL/Model/Guest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Daxus_ClubManagement.DAL.Model
+{
+    internal class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
