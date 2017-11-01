3d2619c755b4a19780dd392648ad72051bae44e7
Robin Svahn <robin.svahn@noser.com>
2017-11-01 13:13:29 +0100

    Added Repository Interface

---
+++ Daxus_ClubManagement_Core#2/Daxus_ClubManagement.DAL/Repositories/Interfaces/IGuestRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Daxus_ClubManagement.DAL.Model;
+
+namespace Daxus_ClubManagement.DAL.Repositories.Interfaces
+{
+    public interface IGuestRepository : IDisposable
+    {
+        IEnumerable<Guest> GetStudents();
+        Guest GetStudentByID(int studentId);
+        void InsertStudent(Guest student);
+        void DeleteStudent(int studentID);
+        void UpdateStudent(Guest student);
+        void Save();
+    }
+}
