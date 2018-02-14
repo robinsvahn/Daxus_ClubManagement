04d6ce3f3d4d8f89a3c70da9fe64e9ce10530bf6
Robin Svahn <robin.svahn@noser.com>
2018-02-14 09:53:00 +0100

    Added Interface for a teamrepository

---
+++ Daxus_FootballManagement_Core#2/Daxus_FootballManagement.DAL/Repositories/Interfaces/Generic/ITeamRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Daxus_FootballManagement.DAL.Model;
+
+namespace Daxus_FootballManagement.DAL.Repositories.Interfaces.Generic
+{
+    public interface ITeamRepository : IGenericRepository<Team>
+    {
+    }
+}
